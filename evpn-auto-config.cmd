4f0040e3bb6181324faa55fab8a3442a59f79a60
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2023-03-10 11:57:35 -0600

    Add explicit admin-state enable, to make sure user configured URL is used

---
+++ evpn-auto-config.cmd
@@ -0,0 +1,7 @@
+# auto-config for srl-evpn lab leaves & spines
+set /system gnmi-server unix-socket admin-state enable
+set /auto-config-agent igp bgp-unnumbered evpn model asymmetric-irb auto-lags encoded-ipv6 bgp-peering ipv4
+set /auto-config-agent gateway ipv4 10.0.0.1/24
+
+set /netbox-agent netbox-url http://172.20.20.12:8000
+set /netbox-agent admin-state enable
